6ef365f3aae8c0d877fcdc973ac5c365ba2d4acd
Emma Reed <ereed@easitpa.com>
2024-02-08 15:14:20 -0700

    first commit

---
+++ src/pages/AppLayout.jsx
@@ -0,0 +1,12 @@
+import AppNav from "../components/AppNav";
+
+function AppLayout() {
+  return (
+    <div>
+      <AppNav />
+      <p>hello welcome yes</p>
+    </div>
+  );
+}
+
+export default AppLayout;
